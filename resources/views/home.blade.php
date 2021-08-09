9e3034e2a13188cd8146f4168829c785c18821ed
ArafaAbRahim <arafaarahim4@gmail.com>
2021-08-09 11:48:02 +0600

    Make slider dynamic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,30 +51,20 @@
                 @endif               
             </div>
             
-            <div class="row tm-welcome-row-2">
-                <div class="col-lg-4 tm-dotted-box-container">
-                    <article class="tm-dotted-box">
-                        <i class="fas fa-3x fa-binoculars tm-article-icon"></i>
-                        <h3 class="tm-article-title">Template Usage</h3>
-                        <p class="tm-article-text">You can feel free to edit and use New Vision template for your commercial websites. Title color is #333</p>
-                        <a class="tm-btn tm-btn-rounded tm-article-link" href="#">More Details</a>
-                    </article>    
-                </div>
-                <div class="col-lg-4 tm-dotted-box-container">
-                    <article class="tm-dotted-box">
-                        <i class="fas fa-3x fa-microscope tm-article-icon"></i>
-                        <h3 class="tm-article-title">New Vision</h3>
-                        <p class="tm-article-text"><a rel="nofollow" target="_parent" href="https://templatemo.com/tm-542-new-vision">New Vision</a> comes with 4 different HTML pages and provided free of charge by TemplateMo. You can add more pages if you need. Text color is #666</p>
-                    </article>    
-                </div>
-                <div class="col-lg-4 tm-dotted-box-container">
-                    <article class="tm-dotted-box">
-                        <i class="fas fa-3x fa-glasses tm-article-icon"></i>
-                        <h3 class="tm-article-title">Download Sites</h3>
-                        <p class="tm-article-text">Do not re-distribute our template ZIP file on your website. Or contact us first. Button color is #C99</p>
-                        <a class="tm-btn tm-article-link" href="#">Read More...</a>
-                    </article>
-                </div>
+            <div class="row tm-welcome-row-2">                
+                @if($post)
+                    @foreach($post as $value)
+                        @if($value->section_title == 'third_section')
+                            <div class="col-lg-4 tm-dotted-box-container">
+                                <article class="tm-dotted-box">
+                                    <img src="{{ asset('uploads') }}/{{$value->image}}" alt="Welcome image" class="img-fluid tm-media-img" style="width: 30%;" />
+                                    <h3 class="tm-article-title">{{$value->title}}</h3>
+                                    <p class="tm-article-text">{!! $value->description !!}</p>                                    
+                                </article> 
+                            </div>
+                        @endif
+                    @endforeach
+                @endif                                       
             </div>
         </section>
         
@@ -96,54 +86,18 @@
             
             <!-- Carousel -->
             <div class="grid tm-carousel">
-                <figure class="effect-honey">
-                    <img src="{{ asset('img/gallery-item-01.jpg') }}" alt="Featured Item">
-                    <figcaption>
-                        <h4><i><span>Best</span> HTML Template</i></h4>
-                    </figcaption>
-                </figure>
-                <figure class="effect-honey">
-                    <img src="{{ asset('img/gallery-item-02.jpg') }}" alt="Featured Item">
-                    <figcaption>
-                        <h4><i><span>Top</span> CSS Theme</i></h4>
-                    </figcaption>
-                </figure>
-                <figure class="effect-honey">
-                    <img src="{{ asset('img/gallery-item-03.jpg') }}" alt="Featured Item">
-                    <figcaption>
-                        <h4><i><span>Best</span> HTML Template</i></h4>
-                    </figcaption>
-                </figure>
-                <figure class="effect-honey">
-                    <img src="{{ asset('img/gallery-item-04.jpg') }}" alt="Featured Item">
-                    <figcaption>
-                        <h4><i><span>Great</span> Web Theme</i></h4>
-                    </figcaption>
-                </figure>
-                <figure class="effect-honey">
-                    <img src="{{ asset('img/gallery-item-05.jpg') }}" alt="Featured Item">
-                    <figcaption>
-                        <h4><i><span>Best</span> HTML Collection</i></h4>
-                    </figcaption>
-                </figure>
-                <figure class="effect-honey">
-                    <img src="{{ asset('img/gallery-item-06.jpg') }}" alt="Featured Item">
-                    <figcaption>
-                        <h4><i><span>Max</span> CSS Layout</i></h4>
-                    </figcaption>
-                </figure>
-                <figure class="effect-honey">
-                    <img src="{{ asset('img/gallery-item-07.jpg') }}" alt="Featured Item">
-                    <figcaption>
-                        <h4><i><span>Best</span> HTML Layout</i></h4>
-                    </figcaption>
-                </figure>
-                <figure class="effect-honey">
-                    <img src="{{ asset('img/gallery-item-08.jpg') }}" alt="Featured Item">
-                    <figcaption>
-                        <h4><i><span>Best</span> CSS Template</i></h4>
-                    </figcaption>
-                </figure>
+                @if($post)
+                    @foreach($post as $value)
+                        @if($value->section_title == 'slider')
+                            <figure class="effect-honey">
+                            <img src="{{ asset('uploads') }}/{{$value->image}}" alt="Welcome image"  />
+                                <figcaption>
+                                    <h4><i >{!! $value->description !!}</i></h4>
+                                </figcaption>
+                            </figure>
+                            @endif
+                    @endforeach
+                @endif                   
             </div>
         </section>
         
